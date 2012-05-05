6b5e1da7c4c3f4f09a9b9e5c8f09f36400871bf1
Paul-Wortmann <paul.wortmann@gmail.com>
2012-05-05 15:39:56 +0900

    Removed "locale" header due to c++ runtime issues on win64 systems.

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@
  * @date 2011-10-01
  */
 
-#include <locale>
+//#include <locale>
 #include <SDL/SDL.h>
 #include <SDL/SDL_main.h>
 #include <SDL/SDL_ttf.h>
@@ -62,7 +62,7 @@ SDL_Surface             *App_Icon_Surface;
 //----------------------------------- Main -------------------------------------
 int main(int argc, char *argv[])
 {
-    std::locale::global( std::locale( "" ) );
+    //std::locale::global( std::locale( "" ) );
     events_init();
     game.log.File_Set("Star.P.G..log");
     game.log.File_Clear();
